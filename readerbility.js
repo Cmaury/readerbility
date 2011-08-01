fb0d5e568bd4b2cb892db653e32ad5d6dfd05783
Chris Maury <chrmaury@gmail.com>
2011-07-31 18:02:04 -0700

    Edit manifest.json to only affect google reader

---
+++ readerbility.js
@@ -0,0 +1,5 @@
+document.getElementById('entry-main').style.backgroundColor = '#343A3F';
+document.getElementById('entry-title').style.
+document.getElementById('entry-author')
+document.getElementById('entry-body')
+document.getElementById('item-body')
