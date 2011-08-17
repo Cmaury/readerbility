5becbd3b69bce6875186f15a90744a52b22d5d8a
Cmaury <chrmaury@gmail.com>
2011-08-17 15:08:31 -0700

    remove js files

--- readerbility.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('entry-main').style.backgroundColor = '#343A3F';
-document.getElementById('entry-title').style.
-document.getElementById('entry-author')
-document.getElementById('entry-body')
-document.getElementById('item-body')
